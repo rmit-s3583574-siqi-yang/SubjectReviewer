4d2a7cd3311eacc894ee74b2839a5f1a3cfc5044
siqi yang <s3583574@student.rmit.edu.au>
2017-08-27 10:09:17 +1000

    Initial Commit

---
+++ SubjectReviewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SubjectReviewer
+//
+//  Created by siqi yang on 27/8/17.
+//  Copyright © 2017 siqi yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
